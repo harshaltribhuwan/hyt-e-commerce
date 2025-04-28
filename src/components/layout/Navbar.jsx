65b942f2022cf437889d51e0c0f3e7ed6ff92032
Harshal Tribhuwan <harshaltribhuwan12345@gmail.com>
2025-04-28 19:38:54 +0530

    done bug fixes in navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -7,11 +7,7 @@ import { motion } from "framer-motion";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
-
-  // Get cartItems from Redux state
   const cartItems = useSelector((state) => state.cart.items);
-
-  // Cart count derived directly from Redux state
   const cartCount = cartItems.length;
 
   const toggleMenu = () => {
@@ -22,6 +18,40 @@ export default function Navbar() {
     <nav className="navbar">
       <div className="navbar__logo">
         <Link to="/">hyt</Link>
+      </div>
+
+      {/* NEW: Mobile Cart + Hamburger */}
+      <div className="navbar__mobile-icons">
+        <Link to="/cart" className="cart-icon-mobile">
+          <FiShoppingCart size={24} />
+          {cartCount > 0 && <span className="cart-count">{cartCount}</span>}
+        </Link>
+
+        <motion.div
+          className="navbar__toggle"
+          onClick={toggleMenu}
+          animate={{ rotate: isOpen ? 180 : 0 }}
+          transition={{ duration: 0.3 }}
+        >
+          <motion.div
+            className="bar"
+            initial={{ rotate: 0 }}
+            animate={{ rotate: isOpen ? 45 : 0, translateY: isOpen ? 7 : 0 }}
+            transition={{ duration: 0.3 }}
+          ></motion.div>
+          <motion.div
+            className="bar"
+            initial={{ opacity: 1 }}
+            animate={{ opacity: isOpen ? 0 : 1 }}
+            transition={{ duration: 0.3 }}
+          ></motion.div>
+          <motion.div
+            className="bar"
+            initial={{ rotate: 0 }}
+            animate={{ rotate: isOpen ? -45 : 0, translateY: isOpen ? -7 : 0 }}
+            transition={{ duration: 0.3 }}
+          ></motion.div>
+        </motion.div>
       </div>
 
       <div className={`navbar__links ${isOpen ? "open" : ""}`}>
@@ -37,37 +67,13 @@ export default function Navbar() {
         <Link to="/contact" onClick={() => setIsOpen(false)}>
           Contact
         </Link>
+
+        {/* Desktop Cart Icon */}
         <Link to="/cart" onClick={() => setIsOpen(false)} className="cart-icon">
           <FiShoppingCart size={24} />
           {cartCount > 0 && <span className="cart-count">{cartCount}</span>}
         </Link>
       </div>
-
-      <motion.div
-        className="navbar__toggle"
-        onClick={toggleMenu}
-        animate={{ rotate: isOpen ? 180 : 0 }}
-        transition={{ duration: 0.3 }}
-      >
-        <motion.div
-          className="bar"
-          initial={{ rotate: 0 }}
-          animate={{ rotate: isOpen ? 45 : 0, translateY: isOpen ? 7 : 0 }}
-          transition={{ duration: 0.3 }}
-        ></motion.div>
-        <motion.div
-          className="bar"
-          initial={{ opacity: 1 }}
-          animate={{ opacity: isOpen ? 0 : 1 }}
-          transition={{ duration: 0.3 }}
-        ></motion.div>
-        <motion.div
-          className="bar"
-          initial={{ rotate: 0 }}
-          animate={{ rotate: isOpen ? -45 : 0, translateY: isOpen ? -7 : 0 }}
-          transition={{ duration: 0.3 }}
-        ></motion.div>
-      </motion.div>
     </nav>
   );
 }
